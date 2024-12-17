5e46a52aaf7afe14f0746f4bb821584955e8b036
Trond Ove Høie <trond.ove.hoie@gmail.com>
2024-12-17 21:12:17 +0100

    Trøbbel med endring i vector

--- main.cpp
+++ main.cpp
@@ -5,6 +5,36 @@
     #include <sstream>
     #include <ranges>
 
+bool fixPrintOrderREC(const std::vector<std::vector<int>>& por, std::vector<int> ptpLine, int n){
+    //print ut ptpLine
+//    for (auto x:ptpLine)std::cout<<x<<" "; 
+//    std::cout<<std::endl; 
+    //
+    int first= por[n][0];
+    int last = por[n][1];
+    auto itFirst = std::ranges::find(ptpLine, first); 
+    auto itLast = std::ranges::find(ptpLine, last);  
+    if ( itFirst==ptpLine.end()||itLast==ptpLine.end()||itFirst<itLast){
+       
+  //      if(itFirst==ptpLine.end())std::cout<<" No First "<<std::endl;
+  //      if(itLast==ptpLine.end())std::cout<<" No Last "<<std::endl; 
+  //      if(itFirst<itLast)std::cout<<" Rule Confirmed "<<std::endl;
+        if (n==0){
+            std::cout<<" Print order apply to all rules \n";
+            return true; 
+        } 
+        n--;
+        return fixPrintOrderREC(por, ptpLine, n);
+    }
+    std::cout<<"ERROR: n="<<n<<std::endl;
+        for (auto x:ptpLine)std::cout<<x<<" "; 
+        std::iter_swap(itFirst, itLast);
+        std::cout<<"After Swap: "<<std::endl;
+        for (auto x:ptpLine)std::cout<<x<<" "; 
+        std::cout<<std::endl;
+    return fixPrintOrderREC(por, ptpLine, n);     
+    }
+
 bool checkPrintOrderREC(const std::vector<std::vector<int>>& por, const std::vector<int>& ptpLine, int n){
     //print ut ptpLine
 //    for (auto x:ptpLine)std::cout<<x<<" "; 
@@ -26,7 +56,7 @@ bool checkPrintOrderREC(const std::vector<std::vector<int>>& por, const std::vec
         n--;
         return checkPrintOrderREC(por, ptpLine, n);
     }
-    std::cout<<"ERROR: n="<<n<<std::endl;
+    std::cout<<"ERROR: n="<<n<<"first = "<<first<<" last = "<<last<<std::endl;
     return false;     
     }
 
@@ -41,7 +71,7 @@ int main(){
    
 
 
-    std::string filename = "C:/Users/trond/Documents/Workspace GIT/CPP Repositories/AoC2024/docs/PageOrderingRules.txt"; 
+    std::string filename = "C:/Users/trond/Documents/Workspace GIT/CPP Repositories/AoC2024/docs/testPageOrderingRules.txt"; 
     std::ifstream infile(filename);
     if (!infile) {
         std::cerr << "Error: Could not open file " << filename << std::endl;
@@ -61,7 +91,7 @@ int main(){
             } 
         }
    
-    std::string filename2 = "C:/Users/trond/Documents/Workspace GIT/CPP Repositories/AoC2024/docs/PagesToProduce.txt"; 
+    std::string filename2 = "C:/Users/trond/Documents/Workspace GIT/CPP Repositories/AoC2024/docs/testPagesToProduce.txt"; 
     std::ifstream infile2(filename2);
     if (!infile2) {
         std::cerr << "Error: Could not open file " << filename2 << std::endl;
@@ -79,19 +109,42 @@ int main(){
         ProductionList.push_back(v); 
     }   
 // All data imported
+    std::vector<int> failedOrders; 
+    int okOrders=0;
+    int adjustedOrders=0;  
     int sumCenterCell=0;
     int nPOR= PageOrderingRules.size()-1; 
     for(int x=0; x<ProductionList.size(); x++){
-        std::cout<<" Production list size: "<<ProductionList.size()<<" x:"<<x<<" LIST: "<<std::endl;
-        for (auto x:ProductionList[x])std::cout<<x<<" "; 
-        std::cout<<std::endl;
+        //std::cout<<" Production list size: "<<ProductionList.size()<<" x:"<<x<<" LIST: "<<std::endl;
+        //for (auto x:ProductionList[x])std::cout<<x<<" "; 
+        //std::cout<<std::endl;
         if(checkPrintOrderREC(PageOrderingRules,ProductionList[x], nPOR)){
+            okOrders++;
             int centercell= ((ProductionList[x].size()/2));
             sumCenterCell = sumCenterCell+ProductionList[x][centercell]; 
 //            std::cout<<"correctly ordered update at line: "<<x<<std::endl;
 //            std::cout<<"Center page no: "<<centercell<<" Center value "<<ProductionList[x][centercell]<<" |Sum: "<<sumCenterCell<<std::endl;
+        } else{
+          failedOrders.push_back(x); 
         }
     }
     std::cout<<" Sum center element approved print orders= "<< sumCenterCell<<std::endl; 
+   
+    std::cout<<" Number of OK orders = "<< okOrders<<std::endl; 
+    std::cout<<" Number of failed orders = "<< failedOrders.size()<<std::endl; 
+    sumCenterCell=0; 
+    for (auto x:failedOrders)std::cout<<x<<" "; 
+    for (auto x:failedOrders){
+
+    while (!fixPrintOrderREC(PageOrderingRules,ProductionList[x], nPOR)); 
+    adjustedOrders++;
+    int centercell= ((ProductionList[x].size()/2));
+    sumCenterCell = sumCenterCell+ProductionList[x][centercell]; 
+    for (auto x:ProductionList[x])std::cout<<x<<" ";      
+    std::cout<<"CenterValue: "<<ProductionList[x][centercell]<<std::endl;
+
+
+    }   
+    std::cout<<" Sum center element adjusted print orders= "<< sumCenterCell<<std::endl; 
     return 0;
 }
